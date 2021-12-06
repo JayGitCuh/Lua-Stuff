05df535381d715eb0dde06a3c37bc7ae39c2a5ed
Jay <94145603+JayGitCuh@users.noreply.github.com>
2021-12-05 23:08:16 -0800

    Update main.lua

--- LuridFramework/Roblox/init/main.lua
+++ LuridFramework/Roblox/init/main.lua
@@ -1,11 +1,2 @@
 --init
-
-function loadScript(path)
-    loadfile(path)()
-end
-
-loadScript("LuridFramework//Universal//library.lua") --just put your path could also do more if you want idc its your script
-loadScript("LuridFramework//Universal//functions.lua")
-
-doFun()
-doLib()
+--your main script here
