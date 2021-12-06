6fac727f036532e80b3340af2795c14ecf0cc6fb
JayGitCuh <94145603+JayGitCuh@users.noreply.github.com>
2021-12-05 21:42:47 -0800

    Add files via upload

---
+++ LuridFramework/Roblox/init/functions.lua
@@ -0,0 +1,5 @@
+--init
+
+function doFun()
+    print("functions loaded")
+end
---
+++ LuridFramework/Roblox/init/main.lua
@@ -0,0 +1,11 @@
+--init
+
+function loadScript(path)
+    loadfile(path)()
+end
+
+loadScript("LuridFramework//Universal//library.lua") --just put your path could also do more if you want idc its your script
+loadScript("LuridFramework//Universal//functions.lua")
+
+doFun()
+doLib()
